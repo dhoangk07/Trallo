88ecd13394150e1d62705f75e27d0906c288e491
dhoang <dhoangk07@gmail.com>
2017-11-14 00:50:55 +0700

    User can edit his comments on modal https://trello.com/c/126Gdp78/44-user-can-edit-his-comments-on-modal

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,32 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+
+    respond_to do |format|
+      format.html { redirect_to card_path(@comment.card_id) }
+      format.js
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(comment_params)
+      respond_to do |format|
+        format.html { redirect_to card_path(@comment.card_id) }
+        format.js
+      end
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+   
+  end
+
   private
 
   def comment_params
