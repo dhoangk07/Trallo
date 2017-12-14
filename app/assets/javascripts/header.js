784caf930d6f680f59724621171be8ffc1904406
dhoang <dhoangk07@gmail.com>
2017-12-14 11:47:18 +0700

    Reallocation items on header by using Popover.

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
 	$('#user-profile').webuiPopover({url:'#user-profile-content'});
+	$('#icon-image').webuiPopover({url:'#icon-image-content'});
+	$('#event-icon').webuiPopover({url:'#event-detail-content'});
+	$('#member-icon').webuiPopover({url:'#member-invite-icon-content'});
 })
